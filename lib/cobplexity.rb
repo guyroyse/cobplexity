93fab39eea9e2326f31ebc2681e4a9bc4bd281d7
Guy Royse <guy@guyroyse.com>
2011-11-29 20:49:00 -0600

    Handles continuations and other column 7 characters

--- lib/cobplexity.rb
+++ lib/cobplexity.rb
@@ -1,25 +1,36 @@
-class Module
-  attr_reader :code, :lines
-  def code= code
-    @code = code
-    @lines = @code.lines.count do |line|
-      cobol_line = Line.new line
-      !cobol_line.blank? && !cobol_line.comment?
+module Cobplexity
+
+  class Module
+    attr_reader :code, :lines
+    def code= code
+      @code = code
+      @lines = @code.lines.count do |line|
+        cobol_line = Line.new line
+        !cobol_line.blank? && !cobol_line.comment? && !cobol_line.continuation? && !cobol_line.procedure_division_header?
+      end
     end
   end
-end
 
-class Line
-  def initialize line
-    @line = line
+  class Line
+    def initialize line
+      @line = line
+    end
+    def blank?
+      @line.strip.empty? || (@line.strip.length < 7) 
+    end
+    def comment?
+      comment_char = ' '
+      comment_char = @line[6] unless self.blank?
+      comment_char == '*'
+    end
+    def continuation?
+      comment_char = ' '
+      comment_char = @line[6] unless self.blank?
+      comment_char == '-'
+    end
+    def procedure_division_header?
+      @line.match /PROCEDURE DIVISION/
+    end
   end
-  def blank?
-    @line.strip.empty? || (@line.strip.length < 7) 
-  end
-  def comment?
-    comment_char = ''
-    comment_char = @line[6] unless self.blank?
-    !comment_char.strip.empty?
-  end
-end
 
+end
--- spec/cobplexity_spec.rb
+++ spec/cobplexity_spec.rb
@@ -1,6 +1,6 @@
 require 'cobplexity'
 
-describe Module do
+describe Cobplexity::Module do
 
   it 'has code' do
     subject.code = 'foo'
@@ -39,6 +39,33 @@ describe Module do
 100000     MOVE 'Y' to YES.
 100010*    CALL MOVE-IT.
 100020     CALL MOVE-IT-AGAIN.
+    eos
+    subject.lines.should == 2
+  end
+
+  it "doesn't count continuations" do
+    subject.code = <<-eos
+100000     MOVE 'Y' to YES.
+100010     CALL
+100020-       MOVE-IT.
+    eos
+    subject.lines.should == 2
+  end  
+
+  it "counts lines with other characters in column 7" do
+    subject.code = <<-eos
+100000     MOVE 'Y' to YES.
+100010/    CALL MOVE-IT.
+100020     CALL MOVE-IT-AGAIN.
+    eos
+    subject.lines.should == 3
+  end  
+
+  it "doesn't count the PROCEDURE DIVISION statement" do
+    subject.code = <<-eos
+100000 PROCEDURE DIVISION.
+100010     MOVE 'Y' to YES.
+100020     CALL MOVE-IT.
     eos
     subject.lines.should == 2
   end
